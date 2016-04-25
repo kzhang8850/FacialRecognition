c0ff5a1165bc4f8c324a31813cf7c8336fd067a0
Kevin Zhang <kzhang1@olin.edu>
2016-04-25 02:28:03 -0400

    All algorithms working

--- ImgCorrelationAlg.m
+++ ImgCorrelationAlg.m
@@ -4,39 +4,80 @@
 
 load classdata.mat;
 
+%creating the testset and the dataset
+testdata = zeros(360, 256, 43);
+counter = 1;
+for i=1:8:344
+    testdata(:,:,counter) = classdata(:,:,i);
+    counter = counter + 1;
+end
 
-%get images and resizing to smaller size
-image1 = classdata(:,:,1);
-img1smallx = resample(image1, 1,4);
-img1small = resample(img1smallx', 1,4);
-image1 = img1small';
-% imshow(image1);
+tempdata = zeros(360, 256, 344-size(testdata, 3));
+counter = 1;
+counter2 = 1;
+for i =1:344
+    if counter < 44
+        if(classdata(:,:,i) == testdata(:,:,counter))
+            counter = counter + 1;
+        else
+            tempdata(:,:,counter2) = classdata(:,:,i);
+            counter2 = counter2 + 1;
+        end
+    else
+        tempdata(:,:,counter2) = classdata(:,:,i);
+        counter2 = counter2 + 1;
+    end
+end
 
-image2 = classdata(:,:,2);
-img2smallx = resample(image2, 1,4);
-img2small = resample(img2smallx', 1,4);
-image2 = img2small';
-% imshow(image2); 
+classdata = tempdata;
 
-image3 = classdata(:,:,20);
-img3smallx = resample(image3, 1,4);
-img3small = resample(img3smallx', 1,4);
-image3 = img3small';
-% imshow(image3);
+correl = 0;
+recognized_image = [];
+
+for i=1:size(testdata, 3)
+    image1 = testdata(:,:,i);
+    
+    %get images and resizing to smaller size
+    
+    img1smallx = resample(image1, 1,4);
+    img1small = resample(img1smallx', 1,4);
+    image1 = img1small';
+    for j=1:size(classdata, 3)
+        image2 = classdata(:,:,j);
+
+        img2smallx = resample(image2, 1,4);
+        img2small = resample(img2smallx', 1,4);
+        image2 = img2small';
 
 
-%reshaping to vectors
-col1 = reshape(image1, [90*64,1]);
-col2 = reshape(image2, [90*64,1]);
-col3 = reshape(image3, [90*64,1]);
+        %reshaping to vectors
+        col1 = reshape(image1, [90*64,1]);
+        col2 = reshape(image2, [90*64,1]);
 
+        %making correlation matrix
+        testmat = [col1 col2];
+        o = ones(size(testmat, 1), 1);
+        m = o * mean(testmat);
+        s = o * std(testmat);
+        b = (testmat-m)./s;
+        c = (1/(size(testmat,1)-1))*b'*b;
+        
+        if c(1,2) > correl
+            correl = c(1,2);
+            recognized_image = image2;
+        end
+    end
+            
+        
+    % plotting result
+    figure;
+    subplot(1,2,1);
+    imshow(image1);
+    xlabel('original');
+    subplot(1,2,2);
+    imshow(recognized_image);
+    xlabel('found');
+    recognized_image = [];
+    correl = 0;       
 
-%making correlation matrix
-testmat = [col1 col2 col3];
-o = ones(size(testmat, 1), 1);
-m = o * mean(testmat);
-s = o * std(testmat);
-b = (testmat-m)./s;
-c = (1/(size(testmat,1)-1))*b'*b;
-display(c);
- 
+end
--- SpaceDistAlg.m
+++ SpaceDistAlg.m
@@ -3,94 +3,59 @@
 
 load classdata.mat;
 
-%reshaping images
-imageone = classdata(:,:, 150);
-imagetwo = classdata(:,:,100);
-newpic1 = reshape(imageone, [360*256,1]);
-newpic2 = reshape(imagetwo, [360*256,1]);
-newpic2 = newpic2 .* 1.1;
+%creating the testset and the dataset
+testdata = zeros(360, 256, 43);
+counter = 1;
+for i=1:8:344
+    testdata(:,:,counter) = classdata(:,:,i);
+    counter = counter + 1;
+end
 
+tempdata = zeros(360, 256, 344-size(testdata, 3));
+counter = 1;
+counter2 = 1;
+for i =1:344
+    if counter < 44
+        if(classdata(:,:,i) == testdata(:,:,counter))
+            counter = counter + 1;
+        else
+            tempdata(:,:,counter2) = classdata(:,:,i);
+            counter2 = counter2 + 1;
+        end
+    else
+        tempdata(:,:,counter2) = classdata(:,:,i);
+        counter2 = counter2 + 1;
+    end
+end
 
-%calculating the stuff for data
-avg1 = sum(newpic1)/(360*256);
-deviation1 = std(newpic1);
-avg2 = sum(newpic2)/(360*256);
-deviation2 = std(newpic2);
-% display(avg1);
-% display(deviation1);
-% display(avg2);
-% display(deviation2);
+classdata = tempdata;
 
-
-%reshaping again
-imageone2 = reshape(newpic1, [360, 256]);
-imagetwo2 = reshape(newpic2, [360, 256]);
-% 
-% imageone = Trans1(imageone2, 5);
-% imagetwo = Trans2(imagetwo2);
-
-% histogram(newpic1);
-% subplot(1,2,1);
-% imshow(imageone);
-% subplot(1,2,2);
-% imshow(imagetwo);
-% histogram(newpic2);
-
-%     diff1 = imageone - imagetwo;
-
-
-%     distance1 = sqrt(sum(sum(diff1.^2)));
-
-
-
-%      display(distance1);
-
-
-%resizing to make smaller
-    img1smallx = resample(imageone, 1,4);
-    img1small = resample(img1smallx', 1,4);
-    imageone = img1small';
-    
-    img2smallx = resample(imagetwo, 1,4);
-    img2small = resample(img2smallx', 1,4);
-    imagetwo = img2small';
-    
-%reshaping into a vector
-    col1 = reshape(imageone, [90*64, 1]);
-    col2 = reshape(imagetwo, [90*64, 1]);
-   
-
-%finding correlation matrix
-    testmat = [col1, col2];
-    o = ones(size(testmat, 1),1);
-    m = o * mean(testmat);
-    s = o * std(testmat);
-    b = (testmat-m)./s;
-    c = (1/(size(testmat,1)-1))*b'*b;
-%     display(c);
-
-
-%target image and initializing variables
-chicken = classdata(:,:,162);
+%initializing variables
 smallest_distance = 100000000000000000000;
 recognized_image = [];
 
 %searching for the differences and finding the smallest distance between
 %two pixels
-for i=1:344
-    temp = classdata(:,:,i);
-    diff = chicken - temp;
-    distance = sqrt(sum(sum(diff.^2)));
-    if distance < smallest_distance
-        recognized_image = temp;
-        smallest_distance = distance;
+for j=1:size(classdata, 3)
+    testing = classdata(:,:,j);
+    for i=1:size(testdata, 3)
+        temp = testdata(:,:,i);
+        diff = testing - temp;
+        distance = sqrt(sum(sum(diff.^2)));
+        if distance < smallest_distance
+            recognized_image = temp;
+            smallest_distance = distance;
+        end
     end
-end
 
-% plotting result
-subplot(1,2,1);
-imshow(chicken);
-xlabel('original');
-subplot(1,2,2);
-imshow(recognized_image);
-xlabel('found');
+    % plotting result
+    figure;
+    subplot(1,2,1);
+    imshow(testing);
+    xlabel('original');
+    subplot(1,2,2);
+    imshow(recognized_image);
+    xlabel('found');
+    recognized_image = [];
+    smallest_distance = 100000000000000000000000000;
+end
